a63b4e0aa3a273ce0b62b71b47d1dec30568f40c
jatin-rajput563 <jkt3298@gmail.com>
2025-03-20 14:32:33 +0530

    adding-back-to-top-button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,16 @@
 const navbarbtn = () => {
     document.getElementById("name").classList.toggle("show-navbar")
     document.body.classList.toggle("overflow-hidden")
+};
+window.addEventListener("scroll", function () {
+    let button = document.getElementById("backToTop");
+    if (window.scrollY > 500) {
+        button.classList.add("show");
+    } else {
+        button.classList.remove("show");
+    }
+});
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
 }
